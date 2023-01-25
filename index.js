25b71e079eb1e4f4f3a6cdd19316f3b3a7d15987
Tony Keiser <keisertony@gmail.com>
2023-01-25 11:18:44 -0800

    Generate share link

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault() // prevent default browser behavior
 
-  const input = document.querySelector('#message')
+  document.querySelector('#message-form').classList.add('hide')
+  document.querySelector('#link-form').classList.remove('hide')
 
-  console.log(input.value)
+  const message = document.querySelector('#message-input')
+  const encrypted = btoa(message.value)
+
+  const shareLink = document.querySelector('#link-input')
+  shareLink.value = `${window.location}#${encrypted}`
+  shareLink.select()
 })
