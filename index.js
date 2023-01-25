a8cd9fb68df17f1090169306b48dbec61abcf94f
Tony Keiser <keisertony@gmail.com>
2023-01-25 11:56:38 -0800

    Decrypt message

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+const { hash } = window.location
+const secretMessage = atob(hash.slice(1)) // remove hash #
+
+if (secretMessage) {
+  document.querySelector('#message-form').classList.add('hide')
+  document.querySelector('#message-show').classList.remove('hide')
+
+  document.querySelector('#message-show p').innerText = secretMessage
+}
+
 document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault() // prevent default browser behavior
 
   document.querySelector('#message-form').classList.add('hide')
   document.querySelector('#link-form').classList.remove('hide')
 
-  const message = document.querySelector('#message-input')
-  const encrypted = btoa(message.value)
+  const messageInput = document.querySelector('#message-input')
+  const encrypted = btoa(messageInput.value)
 
   const shareLink = document.querySelector('#link-input')
   shareLink.value = `${window.location}#${encrypted}`
